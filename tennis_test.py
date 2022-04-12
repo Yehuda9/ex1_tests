11031f873df381a7b6c7b2e80305417545dcd488
yehuda <yehuda7701@gmail.com>
2022-04-12 09:45:27 +0300

    fix

--- tennis_test.py
+++ tennis_test.py
@@ -4,8 +4,8 @@ path_to_script = '../ex1/tennis.sh'
 if __name__ == '__main__':
     r1 = subprocess.run(path_to_script, input=b'5\n10\n5\n10\n15\n10\n15\n10\n10\n10\n',
                         capture_output=True).stdout
-    r2 = subprocess.run('../ex1/tennis.sh', input=b'10\n10\n15\n24\n12\n14\n3\n2\n', capture_output=True).stdout
-    r3 = subprocess.run('../ex1/tennis.sh', input=b'11\n17\n15\n14\n4\n7\n7\n2\n5\n4\n8\n6\n',
+    r2 = subprocess.run(path_to_script, input=b'10\n10\n15\n24\n12\n14\n3\n2\n', capture_output=True).stdout
+    r3 = subprocess.run(path_to_script, input=b'11\n17\n15\n14\n4\n7\n7\n2\n5\n4\n8\n6\n',
                         capture_output=True).stdout
     with open('expected1', 'rb') as f:
         e1 = f.read()
