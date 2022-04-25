f50dc2feb0110a5280d5e35f8d09cf8d9784f973
yehuda <yehuda7701@gmail.com>
2022-04-25 10:23:51 +0300

    compatible with python3.6

--- tennis_test.py
+++ tennis_test.py
@@ -5,10 +5,11 @@ path_to_script = sys.argv[1]
 
 if __name__ == '__main__':
     r1 = subprocess.run(path_to_script, input=b'5\n10\n5\n10\n15\n10\n15\n10\n10\n10\n',
-                        capture_output=True).stdout
-    r2 = subprocess.run(path_to_script, input=b'10\n10\nf\n15\n24\n12\n14\ng\n3\n2\n', capture_output=True).stdout
+                        stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout
+    r2 = subprocess.run(path_to_script, input=b'10\n10\nf\n15\n24\n12\n14\ng\n3\n2\n', stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE).stdout
     r3 = subprocess.run(path_to_script, input=b'11\n17\n15\n14\n4\n7\n7\n2\n5\n4\n8\n6\n',
-                        capture_output=True).stdout
+                        stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout
     with open('expected1', 'rb') as f:
         e1 = f.read()
     with open('expected2', 'rb') as f:
